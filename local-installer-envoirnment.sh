1d53092433789eca913f55a1ed1a23b4de7ef662
Tanvir Ahmad <tanvir@zeptosystems.com>
2016-02-01 17:21:14 +0500

    CleanCode - 001

--- local-installer-envoirnment.sh
+++ local-installer-envoirnment.sh
@@ -7,12 +7,8 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-#PROJECT=$1
 CHECKOUT=$1
 LOCALPROJECT=$2
-#DRUPALV=$4
-#DUSER=$4
-#DPASS=$5
 DBNAME=$3
 DBUSER=$4
 DBPASS=$5
@@ -20,12 +16,8 @@ DBPASS=$5
 if [[ -z $CHECKOUT || -z $LOCALPROJECT || -z $DBNAME || -z $DBUSER || -z $DBPASS ]];
 
   then
-#    echo -e "\e[00;31mPlease Provide Project Name\e[00m"
     echo -e "\e[00;31mPlease Provide a folder to check out the project (example: /home/you/websites/customprojects/) as 8th variable\e[00m"
     echo -e "\e[00;31mPlease Provide Site Name inside CheckOut Folder\e[00m"
-#    echo -e "\e[00;31mPlease Provide DRUPAL Version (e-g 7.23)\e[00m"
-#    echo -e "\e[00;31mPlease Provide Drupal User\e[00m"
-#    echo -e "\e[00;31mPlease Provide Drupal PASS\e[00m"
     echo -e "\e[00;31mPlease Provide DB NAME\e[00m"
     echo -e "\e[00;31mPlease Provide DB USER\e[00m"
     echo -e "\e[00;31mPlease Provide DB PASS\e[00m"
@@ -39,38 +31,13 @@ if [[ -z $CHECKOUT || -z $LOCALPROJECT || -z $DBNAME || -z $DBUSER || -z $DBPASS
 fi
 
 
-#git clone [username]@92.243.15.236:/var/repositories/customprojects/project project.local
 if [ ! -d $CHECKOUT$LOCALPROJECT ]; then
-  #git clone $USERNAME@92.243.15.236:/var/repositories/$TYPE/$PROJECT $CHECKOUT$LOCALPROJECT
   mkdir $CHECKOUT$LOCALPROJECT
   echo -e "\e[00;32m Directory Created $CHECKOUT$LOCALPROJECT  \e[00m"
   cd $CHECKOUT$LOCALPROJECT
-  #drush dl drupal-$DRUPALV
-  #mv drupal-$DRUPALV docroot
   mkdir docroot
   echo -e "\e[00;32m Docroot Directory Created $CHECKOUT$LOCALPROJECT"/docroot"  \e[00m"
   cd $CHECKOUT$LOCALPROJECT"/docroot"
-  #drush site-install standard --account-name=$DUSER --account-pass=$DPASS --db-url=mysql://$DBUSER:$DBPASS@localhost/$DBNAME
-
-#  chown -R $LOCALUSERGROUP $CHECKOUT$LOCALPROJECT
-#  cd $CHECKOUT$LOCALPROJECT
-#  git config user.name "$USERNAME"
-#  git config user.email "$GITEMAIL"
-#  git config color.ui true
-#  git config core.filemode false
-
-#  echo -e "\e[00;32m Site installed Localy  \e[00m"
-#  mkdir "sites/all/modules/contrib"
-#  echo -e "\e[00;32m Contrib Directory Created  \e[00m"
-#  drush dl admin_menu -y
-#  drush dis toolbar -y
-#  drush en admin_menu, admin_menu_toolbar -y
-#  drush dl masquerade -y
-#  drush en masquerade -y
-#  drush dl views -y
-#  drush en views_ui -y
-#  drush dl module_filter -y
-#  drush en module_filter -y
   echo -e "\e[00;32m Site installed Localy  \e[00m"
 else
   echo -e "\e[00;31m $CHECKOUT$LOCALPROJECT exists, skipping checkout creation\e[00m"
@@ -93,7 +60,6 @@ echo "
       allow from all
     </Directory>
   </VirtualHost>" > $APACHEFOLDER/sites-available/$LOCALPROJECT.conf
-#chown $LOCALUSERGROUP $APACHEFOLDER/nodeex-conf/$LOCALPROJECT.conf
 echo -e "\e[00;32mAdded local vhost conf to $APACHEFOLDER/sites-available/$LOCALPROJECT.conf \e[00m"
 
 #Create apachehosts file
